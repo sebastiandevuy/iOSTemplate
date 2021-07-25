0736f08da55e1778cb14b23ebaeb6114b373f628
sebastiandevuy <sebastiandevuy@gmail.com>
2021-07-24 21:50:10 -0300

    Initial commit of project with .gitignore

---
+++ Template/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Template
+//
+//  Created by Pablo Gonzalez on 24/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
